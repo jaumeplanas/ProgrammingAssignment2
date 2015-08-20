5bdb1e1d3400d67433c23ecc0f8f646100dbd314
Jaume Planas <jplanas@itberga.com>
2015-08-20 15:05:57 +0200

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## These functions assume that the matrix supplied is always square and invertible, i.e.
+## they don't include error checking.
+## The computing of the inverse of a square matrix will be done with the solve function in R. 
 
-## Write a short comment describing this function
+## This function creates a special "matrix", which is really a list containing a funcion to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inversed matrix
+## 4. get the value of the inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setinverted <- function(solve) i <<- solve
+	getinverted <- function() i
+	list(set = set, get = get,
+		setinverted = setinverted,
+		getinverted = getinverted)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the special "matrix" created with the above function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getinverted()
+	## Check if exist cached data
+	if(!is.null(i)) {
+		message("getting cached inverted matrix")
+		return(i) # return cached inverted matrix
+	}
+	## If not, cumpute inverted matrix
+	mtrx <- x$get()
+	i <- solve(mtrx, ...)
+	## Cache inverted matrix
+	x$setinverted(i)
+	i ## return computed matrix
 }
